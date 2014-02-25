8280d188f73f0a768870061e74c03c16940f0606
Len <Len@LenmatoMacBook-Pro.local>
2014-02-25 16:31:39 +0800

    Initial Commit

---
+++ OSChina/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OSChina
+//
+//  Created by Len on 14-2-25.
+//  Copyright (c) 2014年 Len. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OSChina/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OSChina
+//
+//  Created by Len on 14-2-25.
+//  Copyright (c) 2014年 Len. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
